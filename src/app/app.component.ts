1d20298add9e13a3702638fef1fd8db1e8db37f7
mariafmedinae <mariafmedinae@gmail.com>
2022-02-18 16:27:22 -0500

    Calling GitHub API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GithubUsersService } from './github-users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'GitHub-user-search-app';
+export class AppComponent implements OnInit {
+  constructor(private githubUsers: GithubUsersService) {}
+
+  ngOnInit() {
+    this.githubUsers.getUser().subscribe(
+      (data) => {console.log(data);}
+    )
+  }
 }
